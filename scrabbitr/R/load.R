4cf144d894004ed7d707d02329ab45ff5030aa5e
Daniel Keitley <dkeitley@gmail.com>
2021-09-21 13:49:51 +0100

    Added temporary R load functions.

--- scrabbitr/R/load.R
+++ scrabbitr/R/load.R
@@ -61,4 +61,107 @@ loadWOTData <- function(wot_path) {
 
 
 
+#' Load macaque data from Yang et al. 2021
+#' @export
+loadYang2021 <- function(data_path) {
 
+  # Load meta
+  meta_raw <- readRDS(paste0(data_path,"metadata_full.Rds"))
+  meta_df <- as.data.frame(bind_rows(meta_raw))
+  rownames(meta_df) <- meta_df$cell
+
+  # Load counts
+  counts <- fread(file=paste0(data_path,"CM_filtered_SoupX_corrected.tsv"),sep="\t",
+                  data.table=F)
+  genes<-counts$V1
+  counts <- counts[,grepl("WT*",colnames(counts))]
+  counts <- sapply(counts,FUN=as.numeric)
+  rownames(counts) <- genes
+  logcounts <- Matrix(counts,sparse=TRUE)
+
+
+  umap_df <- meta_df[colnames(counts),c("UMAP_1","UMAP_2")]
+  meta <- meta_df[colnames(counts),c("cell","lineage")]
+  colnames(meta)[2] <- "celltype"
+
+  # Add stage info
+  meta$stage <- ""
+  meta$stage[grepl("WT_d10_*", meta$cell)] <- "d10"
+  meta$stage[grepl("WT_d12_*", meta$cell)] <- "d12"
+  meta$stage[grepl("WT_d14_*", meta$cell)] <- "d14"
+
+  meta <- meta[,-1]
+
+  sce <- SingleCellExperiment(assays=list(counts=counts),colData=meta,
+                              reducedDims=list(UMAP=umap_df))
+  return(sce)
+}
+
+
+
+#' Load human data from Tyser et al. 2021
+#' @export
+loadTyser2020 <- function(data_path) {
+  X <- readRDS(paste0(data_path,"expression_values.rds"))
+  X_mat <- t(as.matrix(X))
+  colnames(X_mat) <- paste0("cell_",seq(1,ncol(X_mat)))
+  meta <- readRDS(paste0(data_path,"annot_umap.rds"))
+  umap <- as.data.frame(meta[,c("X0","X1")])
+
+  sce <- SingleCellExperiment(assays=list(logcounts = Matrix(X_mat,sparse = TRUE)),
+                              colData = meta,
+                              reducedDims = list(UMAP = umap))
+  sce$stage <- "16dpf"
+
+  colnames(colData(sce))[colnames(meta) == "cluster_id"] <- "celltype"
+  return(sce)
+}
+
+
+
+
+#' @export
+loadRabbitData <- function(data_path, normalise=TRUE) {
+
+  # Load counts
+  sce <- readRDS(paste0(data_path,"normalised_full.RDS"))
+
+  # Load row data
+  genes <- read.csv(paste0(data_path,"features.tsv"),sep="\t",header = F,
+                    col.names = c("ensembl_id","gene_name","feature_type"))
+
+  # Load metadata
+  meta <- read.csv(paste0(data_path,"meta.tab"),
+                   sep = "\t",header = TRUE)
+  rownames(meta) <- meta$index
+  celltypes <- read.csv(paste0(data_path,"annotation_12-07-21.tsv"),sep="\t",
+                        row.names = 1)
+  sce$celltype <-  celltypes[colnames(sce),"updated_celltype"]
+
+  sce$cluster <- meta[,"leiden_2"]
+
+
+  # Load corrected PCs
+  pcs <- data.table::fread(paste0(data_path,"corrected_pcs.tsv"),sep="\t",
+               data.table = F,col.names=c("cell",paste0("PC",seq(1:50))),showProgress = F,fill=T)
+  rownames(pcs) <- pcs$cell
+  pcs <- pcs[,-1]
+  reducedDim(sce,"PCA") <- pcs[colnames(sce),]
+
+
+  # Load umap
+  umap <- read.table(paste0(data_path,"umap.tsv"),sep="\t")
+  rownames(umap) <- meta$index
+  reducedDim(sce,"UMAP") <- umap[colnames(sce),]
+
+  # Apply normalisation
+  if(normalise) {
+    size_factors <- read.csv(paste0(data_path,"size_factors.tsv"),
+                             sep="\t",header = T,  col.names= c("cell","sizeFactors"),row.names = "cell" )
+    sce <- scater::logNormCounts(sce,size_factors=size_factors[colnames(sce),"sizeFactors"])
+
+  }
+
+  return(sce)
+
+}
