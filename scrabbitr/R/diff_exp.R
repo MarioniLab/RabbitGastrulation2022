2edc64656546109909dfc2265e799c7c4fc597fa
Daniel Keitley <dk562@cam.ac.uk>
2022-01-17 20:05:54 +0000

    Fixed NA issue by casting groups to strings.

--- scrabbitr/R/diff_exp.R
+++ scrabbitr/R/diff_exp.R
@@ -10,11 +10,11 @@ prepareEdgeR <- function(sce, group_by, group1, group2, block_by) {
     sce_sub$group <- colData(sce_sub)[[group_by]]
   } else if (group1 == 'All') {
     sce_sub <- sce
-    sce_sub$group <- colData(sce_sub)[[group_by]]
+    sce_sub$group <- as.character(colData(sce_sub)[[group_by]])
     sce_sub$group[sce_sub$group != group2] <- 'All'
   } else if (group2 == 'All') {
     sce_sub <- sce
-    sce_sub$group <- colData(sce_sub)[[group_by]]
+    sce_sub$group <- as.character(colData(sce_sub)[[group_by]])
     sce_sub$group[sce_sub$group != group1] <- 'All'
   }
 
@@ -26,36 +26,41 @@ prepareEdgeR <- function(sce, group_by, group1, group2, block_by) {
 }
 
 
-
 #' Compute differentially expressing genes using EdgeR
 #' @importFrom data.table data.table setnames
 #' @importFrom edgeR glmQLFit estimateDisp topTags glmQLFTest
 #' @importFrom dplyr %>%
+#' @importFrom DelayedArray rowMeans
 #' @export
 runEdgeR <- function(sce, min_detection_rate_per_group=0.1, min.logFC=2,
                      threshold_fdr=0.1) {
 
   # input should already have groups
   groups <- unique(sce$group)
-
+  
   # calculate detection rate per gene
   cdr.dt <- data.table(
     gene = rownames(sce),
-    detection_rate_A = rowMeans(counts(sce[,sce$group==groups[1]])>0),
-    detection_rate_B = rowMeans(counts(sce[,sce$group==groups[2]])>0)
-  ) %>% setnames(c("ens_id",sprintf("detection_rate_%s",groups[1]),sprintf("detection_rate_%s",groups[2])))
+    detection_rate_A = DelayedArray::rowMeans(counts(sce[,sce$group==groups[1]])>0),
+    detection_rate_B = DelayedArray::rowMeans(counts(sce[,sce$group==groups[2]])>0)
+  ) %>% data.table::setnames(c("ens_id",sprintf("detection_rate_%s",groups[1]),sprintf("detection_rate_%s",groups[2])))
 
 
   # Filter genes by min detection rate per group
-  cdr_A <- rowMeans(counts(sce[,sce$group==groups[1]])>0) >= min_detection_rate_per_group
-  cdr_B <- rowMeans(counts(sce[,sce$group==groups[2]])>0) >= min_detection_rate_per_group
+  cdr_A <- DelayedArray::rowMeans(counts(sce[,sce$group==groups[1]])>0) >= min_detection_rate_per_group
+  cdr_B <- DelayedArray::rowMeans(counts(sce[,sce$group==groups[2]])>0) >= min_detection_rate_per_group
   sce <- sce[cdr_B | cdr_A,]
+
+  print(sce)
+  print(head(cdr.dt,10))
+  print(head(cdr_A))
+  print(head(counts(sce)>0))
 
   # Convert SCE to DGEList
   sce_edger <- scran::convertTo(sce, type="edgeR")
 
   # Define design matrix (with intercept)
-  cdr <- colMeans(counts(sce)>0)
+  cdr <- DelayedArray::colMeans(counts(sce)>0)
   design <- model.matrix(~ cdr + sce$batch + sce$group)
 
   # Estimate dispersions
