9f0402e65ff5d5aa2758d31e8b70f442aec23e88
Daniel Keitley <dk562@cam.ac.uk>
2022-02-06 21:49:10 +0000

    Double cell type comparisons working.

--- scripts/differential_expression/run_rabbit_celltypes.sh
+++ scripts/differential_expression/run_rabbit_celltypes.sh
@@ -1,5 +1,7 @@
 #!/usr/local/bin/bash
 
+#./run_rabbit_celltypes.sh -e r_v4 -s run_edgeR.R -d /nfs/research/marioni/dkeitley/RabbitGastrulation2021/data-out/compare_genes/r_downsample.rds -o /nfs/research/marioni/dkeitley/RabbitGastrulation2021/data-out/compare_genes/edgeR_results/rabbit/ -l /nfs/research/marioni/dkeitley/RabbitGastrulation2021/data-out/compare_genes/edgeR_results/rabbit/logs/
+
 while getopts s:e:d:o:l: flag
 do
     case "${flag}" in    
@@ -11,18 +13,32 @@ do
     esac
 done
 
-mapfile -t celltypes < /nfs/research/marioni/dkeitley/RabbitGastrulation2021/data-in/compare_genes/rabbit_celltypes.csv
+
+celltypes=('All' 'Epiblast' 'Primitive Streak' 'Nascent mesoderm' 'Haematoendothelial progenitors' 'Anterior Primitive Streak' 'PGC' 'Allantois endothelium' 'Visceral endoderm' 'Cardiopharyngeal progenitors' 'Embryo proper endothelium' 'EMP' 'Placodal ectoderm' 'Lateral plate mesoderm' 'Notochord' 'NMPs' 'Non-neural ectoderm 4' 'Pharyngeal endoderm' 'Dermomyotome' 'MEP' 'Cranial mesoderm' 'Caudal mesoderm' 'Mesenchyme' 'Anterior somitic tissues' 'Anterior cardiopharyngeal progenitors' 'Cardiomyocytes FHF 1' 'Venous endothelium' 'Erythroid' 'Mesothelium' 'NMPs/Mesoderm-biased' 'Cardiomyocytes FHF 2' 'YS endothelium' 'Sclerotome' 'Presomitic mesoderm' 'Allantois' 'Endocardium' 'Somitic mesoderm' 'Epicardium' 'Posterior somitic tissues' 'Gut tube' 'Nephron progenitors' 'Non-neural ectoderm 2' 'Limb mesoderm' 'Thyroid primordium' 'Migratory neural crest' 'Megakariocytes' 'Mesothelium-endothelium/Masked')
+
+
+# Test of subset of cell types
+#celltypes=("${celltypes[@]:0:5}")
 
 for ia in ${!celltypes[@]}
 do
     for ib in ${!celltypes[@]}
     do
         if [ "$ia" -lt "$ib" ] ; then
-	    stdout=$logdir/${celltypes[$ia]}_vs_${celltypes[$ib]}_stdout.txt
-	    stderr=$logdir/${celltypes[$ia]}_vs_${celltypes[$ib]}_stderr
-	    outfile=$outdir/${celltypes[$ia]}_vs_${celltypes[$ib]}.rds
-            
-	    bsub -M 100000 -o $stdout -e $stderr conda run -n $env Rscript "$script" -s "$sce" -o "$outfile" -g celltype -a "${celltypes[$ia]}" -b "${celltypes[$ib]}" -k sample
+	    #echo ${celltypes[$ia]} ${celltypes[$ib]}
+
+	    celltypeA=${celltypes[$ia]////_}
+	    celltypeB=${celltypes[$ib]////_}
+	    outstem=${celltypeA}_vs_${celltypeB}
+	    outstem=${outstem//[[:blank:]]/_}
+	    #echo $outstem
+
+	    stdout=${logdir}/${outstem}_stdout.txt
+	    stderr=${logdir}/${outstem}_stderr
+	    outfile=${outdir}/${outstem}.rds
+	    #echo $stdout
+
+	    bsub -M 128000 -o $stdout -e $stderr conda run -n $env Rscript "$script" -s "$sce" -o "$outfile" -g celltype -a "${celltypes[$ia]}" -b "${celltypes[$ib]}" -k sample
         fi
     done
 done
