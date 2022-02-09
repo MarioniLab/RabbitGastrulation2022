145b18e499c3da9a6dd7224f5ec1ca28507cb4ad
Daniel Keitley <dkeitley@gmail.com>
2022-02-09 13:01:51 +0000

    Reorganised nhood comparison code.

---
+++ scrabbitr/R/compare_nhoods.R
@@ -0,0 +1,193 @@
+
+#' Average expression values within each neighbourhood
+#' @importFrom Matrix Diagonal
+#' @param X Matrix of expression values
+#' @param mask Indicator matrix specifying cell groupings
+calcNhoodMean <- function(X, mask) {
+
+  ncells <- colSums(mask)
+  ncells_inv <- 1/ncells
+  ncells_mat <- Matrix::Diagonal(x=ncells_inv)
+
+  nhood_sum <- X %*% mask
+  nhood_mean <- nhood_sum %*% ncells_mat
+
+  return(nhood_mean)
+
+}
+
+
+
+#' Calculates gene specificity values
+calcGeneSpec <- function(nhood_mean) {
+  N <- ncol(nhood_mean)
+  col_sums <- colSums(nhood_mean)
+  gspec <- nhood_mean %*% diag(N/col_sums)
+  return(gspec)
+}
+
+
+
+getScranHVGs <- function(sce, n_hvgs, block=NULL) {
+  gene_var <- scran::modelGeneVar(sce, block=NULL)
+  hvgs <- scran::getTopHVGs(gene_var,n=n_hvgs)
+  return(hvgs)
+}
+
+
+selectNhoodFeatures <- function(milo, hvg_selection="scran", max_hvgs=2000,
+                                hvg_block=NULL) {
+
+  # If hvgs already provided
+  if(length(hvg_selection)>1) return(hvg_selection)
+
+  if(!is.null(hvg_block)) {
+    hvg_block <- colData(milo, hvg_block)
+  }
+
+  if(hvg_selection == "scran") {
+    hvgs <- getScranHVGs(milo, max_hvgs, block=hvg_block)
+  }
+
+  return(hvgs)
+}
+
+
+exportNhoodSim <- function(export_dir, r_vals, m_vals, nhood_sim) {
+  r_vals <- as.data.frame(as.matrix(r_vals))
+  m_vals = as.data.frame(as.matrix(m_vals))#
+
+  write.table(r_vals, paste0(export_dir, "r_vals.tsv"), sep="\t", header=TRUE, quote=FALSE)
+  write.table(m_vals, paste0(export_dir, "m_vals.tsv"), sep="\t", header=TRUE, quote=FALSE)
+
+  write.table(nhood_sim, paste0(export_dir, "nhood_sim.tsv"), sep="\t",
+              header=TRUE, quote = FALSE)
+}
+
+
+
+#' @examples
+#' calNhoodSim(r_milo, m_milo, rm_orthologs, hvg_block="sample")
+calcNhoodSim <- function(r_milo, m_milo, orthologs, assay="logcounts",
+                         sim_preprocessing="gene_spec", sim_measure="pearson",
+                         hvg_join_type="intersection", export_dir=NULL, ...) {
+
+  # Check data is normalised
+  if(is.null(logcounts(r_milo)) | is.null(logcounts(m_milo))) {
+    stop("The neighbourhood comparison pipeline requires normalised logcounts.")
+  }
+
+  r_assay <- logcounts(r_milo)
+  m_assay <- logcounts(m_milo)
+
+
+  # Select features
+  r_features <- selectNhoodFeatures(r_milo, ...)
+  m_features <- selectNhoodFeatures(m_milo, ...)
+
+
+  # Combine features
+  if(hvg_join_type == "union") {
+    sim_features <- orthologs[orthologs[,1] %in% r_features |
+                                orthologs[,2] %in% m_features, ]
+  } else if(hvg_join_type == "intersection") {
+    sim_features <- orthologs[orthologs[,1] %in% r_features &
+                                orthologs[,2] %in% m_features, ]
+  } else {
+    stop("Invalid hvg_join_type. Please specify either 'union' or 'intersection'")
+  }
+
+  r_filt <- r_assay[sim_features[,1],]
+  m_filt <- m_assay[sim_features[,2],]
+
+
+  # Average across nhoods
+  r_vals <- calcNhoodMean(r_filt, r_milo@nhoods)
+  m_vals <- calcNhoodMean(m_filt, m_milo@nhoods)
+
+
+  # Sim preprocessing
+  if(!is.null(sim_preprocessing)) {
+    if(sim_preprocessing == "gene_spec") {
+      r_vals <- calcGeneSpec(r_vals)
+      m_vals <- calcGeneSpec(m_vals)
+    } else {
+      stop("Unrecognised sim_processing value. Please specify one of 'gene_spec' or NULL")
+    }
+  }
+
+  colnames(r_vals) <- colnames(r_milo@nhoods)
+  colnames(m_vals) <- colnames(m_milo@nhoods)
+
+
+  # Compute similarity
+  if(sim_measure %in% c("pearson", "kendall", "spearman")) {
+    nhood_sim <- cor(as.matrix(r_vals), as.matrix(m_vals), method=sim_measure)
+  }
+
+  if(!is.null(export_dir)) {
+    exportNhoodSim(export_dir, r_vals, m_vals, nhood_sim)
+  }
+
+
+  return(list(r_vals = r_vals, m_vals = m_vals, nhood_sim = nhood_sim))
+
+}
+
+
+
+subsetMilo <- function(r_milo, r_graph) {
+
+  # Get nhood indices
+  r_nhoodIDs <- as.numeric(vertex_attr(r_graph)$name)
+  r_indCells <- colnames(r_milo)[r_nhoodIDs]
+
+  # Identify cells within r_graph neighbourhoods
+  rhood_hits <- rowSums(r_milo@nhoods[,as.character(r_nhoodIDs)])
+
+  # Milo bug workaround - index cells included in r_milo@nhoods in Milo dev branch
+  rhood_cells <- unique(c(colnames(r_milo)[rhood_hits > 0], r_indCells))
+
+  # Filter original SingleCellExperiment
+  r_milo <- r_milo[,rhood_cells]
+
+  return(r_milo)
+}
+
+
+
+getNhoodPositions <- function(milo, nh_graph=NULL, dimred="UMAP") {
+
+  # Check if graph provided
+  if(is.null(nh_graph)) { nh_graph <- nhoodGraph(milo) }
+
+  # Extract nhood embedding positions
+  nhoodIDs <- as.numeric(vertex_attr(nh_graph)$name)
+  indCells <- vertex_attr(nh_graph)$cell_name
+
+  nhoodPos <- data.frame(reducedDim(milo, dimred)[indCells,1:2])
+  rownames(nhoodPos) <- as.character(nhoodIDs)
+  colnames(nhoodPos) <- c("x","y")
+
+  return(nhoodPos)
+}
+
+
+
+filterMaxMappings <- function(nhood_sim, nhood_axis, long_format=FALSE) {
+
+  if(!long_format) {
+    nhood_sim <- reshape2::melt(as.matrix(nhood_sim))
+    colnames(nhood_sim) <- c("nhoods1", "nhoods2", "sim")
+  }
+
+  df_sim <- nhood_sim[order(nhood_sim[,nhood_axis], -nhood_sim[,3]), ]
+  max_nhoods <- df_sim[!duplicated(df_sim[,nhood_axis]),]
+  rownames(max_nhoods) <- max_nhoods[,nhood_axis]
+  #max_nhoods <- max_nhoods[vertex_attr(r_graph)$name,]
+
+  return(max_nhoods)
+}
+
+
+
---
+++ scrabbitr/R/plot_nhoods.R
@@ -0,0 +1,185 @@
+
+
+
+
+plotNhoodMaxSim <- function(milo, df_maxNhood, colour_by="sim", legend_title="Max correlation" ) {
+
+  nh_graph <- nhoodGraph(milo)
+  V(nh_graph)$max_correlation <- df_maxNhood[vertex_attr(nh_graph)$name, colour_by]
+  layout <- reducedDim(milo, "UMAP")[as.numeric(vertex_attr(nhoodGraph(milo))$name),]
+  colnames(layout) <- c("x","y")
+
+  p <- ggraph(simplify(nh_graph), layout = layout) +
+    ggrastr::rasterise(geom_edge_link0(edge_colour = "grey66", edge_alpha=0.1),dpi=300) +
+    ggrastr::rasterise(geom_node_point(aes(color = max_correlation), size = 3,alpha=0.8, shape=20),dpi=300) +
+    theme_classic(base_size=14) +
+    theme(axis.line = element_blank(), axis.text = element_blank(),
+          axis.ticks = element_blank(), axis.title = element_blank(),
+          aspect.ratio=1) +
+    scale_color_viridis(option="cividis",name="Max correlation")
+
+  return(p)
+
+}
+
+
+
+#' sim_values needs to be in order of milo nhoods
+#' TODO: Split into smaller functions
+plotNhoodSimGroups <- function(milo, sim_values, group_by="celltype",facet_by=NULL, type="ridge", orientation="vertical",colour_by=NULL,size=0.2,
+                               subset=NULL,decreasing=FALSE,rel_min_height=0, show_rank=FALSE, rank_size=2, group_colours=scrabbitr::celltype_colours, xlabel="Cell type", ylabel="Correlation") {
+  graph <- nhoodGraph(milo)
+
+  df <- data.frame(nhood=vertex_attr(graph)$name,
+                   max_sim=sim_values,
+                   group=colData(milo)[as.numeric(vertex_attr(graph)$name), group_by])
+
+  if(!is.null(facet_by)) {
+    df$facet_by <- colData(milo)[as.numeric(vertex_attr(graph)$name), facet_by]
+  }
+
+  if(!is.null(colour_by)) {
+    df$colour_by <- colData(milo)[as.numeric(vertex_attr(graph)$name), colour_by]
+  }
+
+  df_mean <- aggregate(df[,"max_sim"], list(df$group), mean,)
+  df$group <- factor(df$group, levels=df_mean[order(df_mean$x,decreasing=decreasing),"Group.1"])
+
+  df_mean$ranking[order(df_mean$x, decreasing=TRUE)] <- paste0("(",1:nrow(df_mean),"/", nrow(df_mean), ")")
+  rownames(df_mean) <- df_mean$Group.1
+  df$ranking <- df_mean[as.character(df$group), "ranking"]
+
+  if(!is.null(subset)) {
+    show_rank <- TRUE
+    df <- df[df$group %in% subset,]
+    df_mean <- df_mean[df_mean$Group.1 %in% subset,]
+  }
+
+  p <- ggplot(df, aes(x=max_sim,y=group,fill=group,color=colour_by)) + theme_bw()
+
+  if(type=="boxplot") {
+    p <- p + geom_boxplot(outlier.size=0.1)
+
+  } else if(type=="violin") {
+    p <- p + geom_violin()
+
+  } else {
+    p <- p + geom_density_ridges(size=size,rel_min_height = rel_min_height) +
+      theme_ridges(grid = TRUE,center_axis_labels = TRUE)
+
+    if(show_rank) {
+      p <- p + geom_text(data=df_mean, aes(x=max(df$max_sim) + 0.05, y=Group.1, label=ranking, color=),
+                         size = rank_size, inherit.aes = FALSE) +
+        coord_cartesian(xlim = c(min(df$max_sim), max(df$max_sim)),clip = 'off')
+    }
+  }
+
+  if(!is.null(facet_by)) p <- p + facet_grid(cols=vars(facet_by))
+
+  if(orientation=="horizontal") {
+    p <- p + coord_flip()
+    if(!is.null(facet_by)) p <- p + facet_grid(rows=vars(facet_by))
+  }
+
+  p <- p +
+    theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+    scale_fill_manual(values = group_colours[names(group_colours) %in% unique(df$group)], name = "") +
+    guides(fill="none",color=guide_legend(title="",override.aes = list(fill = "white"))) +
+    xlab(xlabel) + ylab(ylabel)
+
+  return(p)
+
+}
+
+
+
+
+
+
+plotNhoodSim <- function(r_milo, m_milo, df_sim, reduced_dim="UMAP", colour_by, r_graph=NULL, m_graph=NULL, r_umap= FALSE, m_umap=FALSE,
+                         offset=c(10,0), reflect.X=FALSE, reflect.Y = FALSE, rotate=NULL, line_alpha=0.05, edge_alpha=0.2,
+                         colours=celltype_colours, legend_pos="none") {
+
+  # Check if graph provided
+  if(is.null(r_graph)) { r_graph <- nhoodGraph(r_milo) }
+  if(is.null(m_graph)) { m_graph <- nhoodGraph(m_milo) }
+
+  # Get nhood embedding positions
+  r_nhoodPos <- getNhoodPositions(r_milo, r_graph)
+  m_nhoodPos <- getNhoodPositions(m_milo, m_graph)
+
+  # TODO: Fix this
+  if(!is.null(rotate)) {
+    rotate <- rotate*pi/180
+    com <- colMeans(m_nhoodPos[,1:2])
+    m_nhoodPos = sweep(m_nhoodPos,2,com, "-")
+    m_nhoodPos[,1] = m_nhoodPos[,1]*cos(rotate) - m_nhoodPos[,2]*sin(rotate)
+    m_nhoodPos[,2] = m_nhoodPos[,1]*sin(rotate) + m_nhoodPos[,2]*cos(rotate)
+    m_nhoodPos = sweep(m_nhoodPos,2,com, "+")
+  }
+  if(reflect.X) { m_nhoodPos[,1] = -m_nhoodPos[,1]}
+  if(reflect.Y) {  m_nhoodPos[,2] = -m_nhoodPos[,2]}
+
+
+  # Add spacing to offset mouse and rabbit umaps
+  m_nhoodPos[,1] <- m_nhoodPos[,1] + offset[1]
+  m_nhoodPos[,2] <- m_nhoodPos[,2] + offset[2]
+
+  r_df <- cbind(r_nhoodPos, vertex_attr(r_graph)[[colour_by]])
+  colnames(r_df) <- c("x","y","obs")
+
+  m_df <- cbind(m_nhoodPos, vertex_attr(m_graph)[[colour_by]])
+  colnames(m_df) <- c("x","y","obs")
+
+  # Plot nhoods and nhood graphs
+  df_plot <- rbind(r_df, m_df)
+  p <- ggplot(df_plot,aes(x=x,y=y)) +
+    geom_edge_link0(data=get_edges(format="short")(create_layout(simplify(r_graph),layout=r_nhoodPos)),
+                    edge_colour = "grey66", edge_alpha=edge_alpha) +
+    geom_edge_link0(data=get_edges(format="short")(create_layout(simplify(m_graph),layout=m_nhoodPos)),
+                    edge_colour = "grey66", edge_alpha=edge_alpha) +
+    geom_point(aes(fill=obs),stroke=0,shape=21) +
+    scale_fill_manual(values = celltype_colours[names(celltype_colours) %in% unique(df_plot$obs)], name = "")
+
+
+  # Filter sim data frame to nhoods within graphs provided
+  r_nhoodIDs <- as.numeric(vertex_attr(r_graph)$name)
+  m_nhoodIDs <- as.numeric(vertex_attr(m_graph)$name)
+
+  sim_filt <- df_sim[(df_sim$r_nhood %in%  r_nhoodIDs) &
+                       (df_sim$m_nhood %in% m_nhoodIDs),]
+
+
+  # Give each line a unique name
+  sim_filt$alignment <- paste0("align_",sim_filt$r_nhood,"_",sim_filt$m_nhood)
+
+  # Link rabbit and mouse nhood positions by alignment name
+  r_lines <- sim_filt
+  r_lines[,c("x","y")] <- r_nhoodPos[as.character(r_lines$r_nhood),]
+
+  m_lines <- sim_filt
+  m_lines[, c("x","y")] <- m_nhoodPos[as.character(m_lines$m_nhood),]
+  df_lines <- rbind(r_lines, m_lines)
+
+
+  # Add similarities lines
+  p <- p + geom_line(data=df_lines,aes(x=x,y=y, group=alignment,colour=sim),alpha=line_alpha) +
+    scale_color_distiller(palette = "Spectral",limits=c(min(df_sim$sim),max(df_sim$sim)))
+
+
+  # Add theme
+  p <- p +  theme_classic(base_size=14) +
+    theme(axis.line = element_blank(), axis.text = element_blank(),
+          axis.ticks = element_blank(), axis.title = element_blank(),
+          legend.position=legend_pos,aspect.ratio=1)
+
+
+  return(p)
+
+}
+
+
+
+
+
+
